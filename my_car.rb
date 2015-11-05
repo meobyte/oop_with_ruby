fae56492bdf08158350c6cf14df935ef93e9ebb2
Michelle Anderson <meobyte@gmail.com>
2015-11-04 20:04:54 -0800

    Classes and Objects I exercises

---
+++ my_car.rb
@@ -0,0 +1,50 @@
+class MyCar
+
+  attr_accessor :color
+  attr_reader :year
+
+  def initialize(color, model, year)
+    @color = color
+    @model = model
+    @year = year
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and speed up by #{number} mph"
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and slow down by #{number} mph"
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def turn_off
+    @current_speed = 0
+    puts "Parking."
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "The car is painted #{color} now."
+  end
+
+end
+
+cavalier = MyCar.new("blue", "chevy cavalier", 1988)
+
+cavalier.speed_up(10)
+cavalier.current_speed
+cavalier.brake(5)
+cavalier.current_speed
+cavalier.turn_off
+cavalier.current_speed
+cavalier.color = "red"
+puts cavalier.color
+puts cavalier.year
+cavalier.spray_paint("sliver")
