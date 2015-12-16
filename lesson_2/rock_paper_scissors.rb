b3531a51094c616afb7b7410bb9974ceb71621f8
Michelle Anderson <meobyte@gmail.com>
2015-12-16 00:16:27 +0000

    play again

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -74,11 +74,28 @@ class RPSGame
     end
   end
 
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n"
+    end
+
+    return true if answer == 'y'
+    return false
+  end
+
+
   def play
     display_welcome_message
-    human.choose
-    computer.choose
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
