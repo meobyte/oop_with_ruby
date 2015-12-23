1c9dd0406402b2b6362034ff21e355118e2e03db
Michelle Anderson <meobyte@gmail.com>
2015-12-23 23:44:36 +0000

    Setup Board and Square classes

--- lesson_5/tic_tac_toe.rb
+++ lesson_5/tic_tac_toe.rb
@@ -1,10 +1,23 @@
 class Board
+  INITIAL_MARKER = ' '
+
   def initialize
+    @squares = {}
+    (1..9).each { |key| @squares[key] = Square.new(INITIAL_MARKER) }
+  end
+
+  def get_square_at(key)
+    @squares[key]
   end
 end
 
 class Square
-  def initialize
+  def initialize(marker)
+    @marker = marker
+  end
+
+  def to_s
+    @marker
   end
 end
 
@@ -17,6 +30,11 @@ class Player
 end
 
 class TTTGame
+  attr_reader :board
+  def initialize
+    @board = Board.new
+  end
+
   def display_welcome_message
     puts "Welcome to Tic Tac Toe!"
     puts ""
@@ -29,15 +47,15 @@ class TTTGame
   def display_board
     puts <<-eos
        |     |
-       |     |
-       |     |
-  -----+-----+-----
-       |     |
-       |     |
+    #{board.get_square_at(1)}  |  #{board.get_square_at(2)}  |  #{board.get_square_at(3)}
        |     |
   -----+-----+-----
        |     |
+    #{board.get_square_at(4)}  |  #{board.get_square_at(5)}  |  #{board.get_square_at(6)}
        |     |
+  -----+-----+-----
+       |     |
+    #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}
        |     |
 
     eos
