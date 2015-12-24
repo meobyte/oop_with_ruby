5289b96af1e826af433cf7502b83b7f0a87b6cd1
Michelle Anderson <meobyte@gmail.com>
2015-12-24 00:02:55 +0000

    Human moves

--- lesson_5/tic_tac_toe.rb
+++ lesson_5/tic_tac_toe.rb
@@ -9,9 +9,15 @@ class Board
   def get_square_at(key)
     @squares[key]
   end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
 end
 
 class Square
+  attr_accessor :marker
+
   def initialize(marker)
     @marker = marker
   end
@@ -22,17 +28,20 @@ class Square
 end
 
 class Player
-  def initialize
-  end
+  attr_reader :marker
 
-  def mark
+  def initialize(marker)
+    @marker = marker
   end
 end
 
 class TTTGame
-  attr_reader :board
+  attr_reader :board, :human, :computer
+
   def initialize
     @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
   end
 
   def display_welcome_message
@@ -61,14 +70,26 @@ class TTTGame
     eos
   end
 
+  def human_moves
+    puts "Choose a square betwee 1-9: "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that's not a valid choice."
+    end
+
+    board.set_square_at(square, human.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
-      first_player_moves
+      human_moves
       break if someone_won? || board_full?
 
-      second_player_moves
+      computer_moves
       break if someone_won? || board_full?
     end
     display_result
